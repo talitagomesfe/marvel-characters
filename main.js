825acc25be04b048c6d70bf5ee2725e47df8ad0e
Talita Gomes Ferreira <talita.ferreira@db1.com.br>
2020-06-04 19:49:38 -0300

    Starting with JS

---
+++ main.js
@@ -0,0 +1,7 @@
+let $characterImage = document.querySelector{'.character-data__image'}
+let $characterDescription = document.querySelector{'.character-data__description'}
+let $searchBox = document.querySelector{'.search__box'}
+
+function changeHero(imgSrc, ) {
+
+}
