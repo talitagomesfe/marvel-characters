37752b64394b53f424a326f235425a401563dee4
Camz <talitagomesfe@gmail.com>
2020-06-05 03:59:10 -0300

    Starting with JS - 2

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-let $characterImage = document.querySelector{'.character-data__image'}
-let $characterDescription = document.querySelector{'.character-data__description'}
-let $searchBox = document.querySelector{'.search__box'}
+var $characterImage = document.querySelector(".character-data__hero-image")
+var $characterDescription = document.querySelector("p.character-data__text")
+var $searchBox = document.querySelector(".search__box")
+var $searchImage = document.querySelector(".search__image")
 
-function changeHero(imgSrc, ) {
-
+$searchImage.onclick = function changeHero() {
+    $characterDescription.innerText = "Character '" + $searchBox.value + "' not found.";
+    $characterImage.src="https://image.freepik.com/vetores-gratis/mulher-bonita-com-ponto-de-interrogacao-acima-da-cabeca_97632-1902.jpg";
 }
