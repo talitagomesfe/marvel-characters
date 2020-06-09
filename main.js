e8534c489e085664c85fbbf36150ff8ecade0243
Talita Gomes Ferreira <talita.ferreira@db1.com.br>
2020-06-08 22:15:22 -0300

    Idk

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-var $characterImage = document.querySelector(".character-data__hero-image")
-var $characterDescription = document.querySelector("p.character-data__text")
-var $searchBox = document.querySelector(".search__box")
-var $searchImage = document.querySelector(".search__image")
+(function init() {
+    var $characterImage = document.querySelector(".character-data__hero-image")
+    var $characterDescription = document.querySelector("p.character-data__text")
+    var $searchBox = document.querySelector(".search__box")
+    var $searchImage = document.querySelector(".search__image")
 
-$searchImage.onclick = function changeHero() {
-    $characterDescription.innerText = "Character '" + $searchBox.value + "' not found.";
-    $characterImage.src="https://image.freepik.com/vetores-gratis/mulher-bonita-com-ponto-de-interrogacao-acima-da-cabeca_97632-1902.jpg";
-}
+    $searchImage.onclick = function changeHero() {
+        $characterDescription.innerText = "Character '" + $searchBox.value + "' not found.";
+        $characterImage.src="https://image.freepik.com/vetores-gratis/mulher-bonita-com-ponto-de-interrogacao-acima-da-cabeca_97632-1902.jpg";
+    }
+
+})();
